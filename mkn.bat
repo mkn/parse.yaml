15e701e992c1ce01d1ac4a3f7035f2a819c6b57c
Dekken <philip.deegan@gmail.com>
2019-03-23 18:59:21 +0100

    Update mkn.bat

--- mkn.bat
+++ mkn.bat
@@ -8,3 +8,7 @@ SET URL="https://github.com/jbeder/yaml-cpp"
 IF NOT EXIST %PWD%\p (
 git clone --depth 1 %URL% -b %VERSION% p --recursive || exit 666 /b
 )
+cd %PWD%\p
+rem issue in yaml-cpp
+git reset HEAD~1 --hard
+
