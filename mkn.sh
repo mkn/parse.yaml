afd7e978b55a887a6f6456d333c542fc791ff9a9
dekken <philip.deegan@gmail.com>
2021-10-16 18:53:19 +0200

    ffs

--- mkn.sh
+++ mkn.sh
@@ -6,11 +6,14 @@ CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 VERSION="master"
 DIR="p"
-GIT_URL="https://github.com/jbeder/yaml-cpp"
+NAME="yaml-cpp"
+GIT_URL="https://github.com/jbeder/${NAME}"
 
 HAS=1
 [ ! -d "$CWD/$DIR" ] && HAS=0 && git clone --depth 1 $GIT_URL -b $VERSION $DIR --recursive
 
 [ $HAS -eq 1 ] && git pull --recurse-submodules origin $VERSION
+
+cp dll.h ${DIR}/include/${NAME}
 
 exit 0
